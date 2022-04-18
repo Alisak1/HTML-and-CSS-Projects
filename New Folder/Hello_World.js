6ff61c9e2f061667f5ee54e86f5764b867202f22
Alisak1 <contactalisa@me.com>
2022-04-17 23:25:07 -0700

    Updated BASIC_HTML_DEMO_1 Assignment
    includes CSS part 2 to 4 videos and favicon

---
+++ New Folder/Hello_World.js
@@ -0,0 +1,12 @@
+var greeting = 'Hello, World!';
+    console.log(greeting);
+
+   var greeting2 = 'Welcome to Node.js';
+console.log(greeting2);
+   
+    var sentence1 = 'Learning how to debug code with the debugger';
+    newFunction();
+
+function newFunction() {
+    console.log(sentence1);
+}
